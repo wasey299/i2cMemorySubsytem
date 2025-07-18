a6bb7283c4d43812b5920355123d36b470786b04
Fardeen Wasey <wasey@mo.ece.pdx.edu>
2025-07-17 18:43:40 -0700

    Initial commit

---
+++ master.sv
@@ -0,0 +1,12 @@
+module master
+(
+input logic rst, clk, rw, /*1:read, 0:write*/ dataValid,
+input logic [6:0] addr,
+input din,
+output dout,
+inout tri sda, scl,
+output busy, ackErr, done
+);
+
+
+endmodule
